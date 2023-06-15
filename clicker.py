abe69f5f22ca5fed4da03cc152c1553df3288d4f
Jonathan <chengjonathan2021@gmail.com>
2023-06-14 21:37:51 -0700

    More Changes

--- clicker.py
+++ clicker.py
@@ -1,18 +1,17 @@
 import pyautogui
 import time
 
-# Set the screen coordinates where you want to click
-x, y = 100, 100
-
 # Set the time interval between clicks (in seconds)
 interval = 0.5
 
-while True:
-    # Move the cursor to the specified screen position
-    pyautogui.moveTo(x, y)
 
-    # Perform the click
-    pyautogui.click()
+while True:
+    # Get the current coordinates of the mouse cursor
+    x, y = pyautogui.position()
+
+    # Perform the click at the current cursor position
+    pyautogui.click(x, y)
 
     # Wait for the specified time interval before the next click
     time.sleep(interval)
+
