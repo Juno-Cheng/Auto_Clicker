9dc0615d6adb03b4675aa9e49673e336d8a55b26
Jonathan <chengjonathan2021@gmail.com>
2023-06-15 19:00:49 -0700

    Final

--- clicker.py
+++ clicker.py
@@ -5,54 +5,88 @@ import tkinter as tk
 # Set the initial time interval (in seconds)
 interval = 0.5
 
+# Define variable for clicking status
+clicking = False
+
 # Function to update the interval when the button is clicked
 def update_interval():
     global interval
-    interval = float(entry.get())
+    hours = int(hours_entry.get())
+    minutes = int(minutes_entry.get())
+    seconds = int(seconds_entry.get())
+    milliseconds = int(milliseconds_entry.get())
+    interval = hours * 3600 + minutes * 60 + seconds + milliseconds / 1000
 
 # Function to start the clicking loop
 def start_clicking():
-    try:
-        while True:
-            # Get the current coordinates of the mouse cursor
-            x, y = pyautogui.position()
+    global clicking
+    clicking = True
+    click()
 
-            # Perform the click at the current cursor position
-            pyautogui.click(x, y)
+def click():
+    if clicking:
+        # Get the current coordinates of the mouse cursor
+        x, y = pyautogui.position()
 
-            # Wait for the specified time interval before the next click
-            time.sleep(interval)
+        # Perform the click at the current cursor position
+        pyautogui.click(x, y)
 
-    except KeyboardInterrupt:
-        # Handle the keyboard interrupt
-        print("Program stopped by user")
+        # Schedule the next click after the specified time interval
+        window.after(int(interval * 1000), click)
+
+def stop_clicking():
+    global clicking
+    clicking = False
 
 # Create the GUI window
 window = tk.Tk()
 window.title("Auto Clicker")
-window.geometry("300x150")
+window.geometry("490x200")
 window.resizable(False, False)
+window.iconbitmap('icon.ico')
 
 # Create the label and entry for the time interval
-label = tk.Label(window, text="Time Interval (in seconds):")
-label.pack()
-entry = tk.Entry(window)
-entry.pack()
+label = tk.Label(window, text="Time Interval:")
+label.grid(row=0, column=0, columnspan=8)
+
+hours_label = tk.Label(window, text="Hours:")
+hours_label.grid(row=1, column=0)
+hours_entry = tk.Entry(window, width=5)
+hours_entry.insert(0, '0')  # Default value
+hours_entry.grid(row=1, column=1)
+
+minutes_label = tk.Label(window, text="Minutes:")
+minutes_label.grid(row=1, column=2)
+minutes_entry = tk.Entry(window, width=5)
+minutes_entry.insert(0, '0')  # Default value
+minutes_entry.grid(row=1, column=3)
+
+seconds_label = tk.Label(window, text="Seconds:")
+seconds_label.grid(row=1, column=4)
+seconds_entry = tk.Entry(window, width=5)
+seconds_entry.insert(0, '0')  # Default value
+seconds_entry.grid(row=1, column=5)
+
+milliseconds_label = tk.Label(window, text="Milliseconds:")
+milliseconds_label.grid(row=1, column=6)
+milliseconds_entry = tk.Entry(window, width=5)
+milliseconds_entry.insert(0, '0')  # Default value
+milliseconds_entry.grid(row=1, column=7)
 
 # Create the button to update the interval
 update_button = tk.Button(window, text="Update Interval", command=update_interval)
-update_button.pack()
+update_button.grid(row=2, column=0, columnspan=8)
 
 # Create the button to start the clicking loop
 start_button = tk.Button(window, text="Start Clicking", command=start_clicking)
-start_button.pack()
+start_button.grid(row=3, column=0, columnspan=8)
 
 # Create the button to stop the clicking loop
-start_button = tk.Button(window, text="Stop Clicking", command=start_clicking)
-start_button.pack()
+stop_button = tk.Button(window, text="Stop Clicking", command=stop_clicking)
+stop_button.grid(row=4, column=0, columnspan=8)
 
 exit_button = tk.Button(window, text="Exit Program", command=window.destroy)
-exit_button.pack()
+exit_button.grid(row=5, column=0, columnspan=8)
 
-# Start the GUI main loop
 window.mainloop()
+
